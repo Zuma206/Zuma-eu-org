86c172d41278e61d0b6c9b50f3cab9dafd6541a8
Zuma <james.peedle@yahoo.com>
2023-08-17 22:57:30 +0100

    Fixed background scaling

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -4,8 +4,11 @@ import background from "@assets/bg.svg";
 export default function Background({ children }: PropsWithChildren) {
   return (
     <section
-      className="w-full bg-zinc-950 bg-cover overflow-scroll"
-      style={{ backgroundImage: `url(${background})` }}
+      className="w-full bg-zinc-950 overflow-scroll"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "100% auto",
+      }}
     >
       {children}
     </section>
