e1045ef109b80b6631c32d4d9428562975358d3e
Zuma <james.peedle@yahoo.com>
2023-08-22 13:01:23 +0100

    Hooked repo code up to API

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,7 @@
-import data from "./data";
 import type { Repo } from "./repo";
 
 export async function fetchRepos() {
-  // const response = await fetch("https://api.github.com/users/Zuma206/repos");
-  // const repos = await response.json();
-  return data as Repo[];
+  const response = await fetch("https://api.github.com/users/Zuma206/repos");
+  const repos = await response.json();
+  return repos as Repo[];
 }
