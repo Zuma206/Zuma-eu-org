1586d9b860dd4f1bf9d6570b4184e3b7ba68d0b8
Zuma <james.peedle@yahoo.com>
2023-08-22 22:32:35 +0100

    Removed netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import netlify from "@astrojs/netlify/functions";
-
 // https://astro.build/config
 export default defineConfig({
   output: "static",
   integrations: [react(), tailwind()],
-  adapter: netlify()
 });
