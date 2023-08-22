4652fad4b48a3ff012e4d5183f07dccf952c8da7
Zuma <james.peedle@yahoo.com>
2023-08-22 19:14:00 +0100

    Removed default github data

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,11 +1,10 @@
-import data from "./data";
 import type { Repo } from "./repo";
 
 const url = "https://api.github.com/users/Zuma206/repos";
 
 export async function fetchRepos() {
   const response = await fetch(url);
-  if (!response.ok) return data;
+  if (!response.ok) throw new Error(`Failed to fetch ${url}`);
   const repos = await response.json();
   return repos as Repo[];
 }
